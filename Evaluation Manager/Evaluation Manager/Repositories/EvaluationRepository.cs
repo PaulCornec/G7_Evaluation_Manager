8fe796c4faa2f1f24050f920ea0b3e9d362d6dff
Paul CORNEC <cornec@et.esiea.fr>
2023-05-16 13:09:39 +0200

    Laboratory Work - 16/05

---
+++ Evaluation Manager/Evaluation Manager/Repositories/EvaluationRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Evaluation_Manager.Models;
+using System.Data.SqlClient;
+using DBLayer;
+
+namespace Evaluation_Manager.Repositories
+{
+    public class EvaluationRepository
+    {
+        public static Evaluation GetEvaluation(Student student, Activity activity)
+        {
+            Evaluation evaluation = null;
+            string sql = $"SELECT * FROM Evaluations" + $"WHERE IdStudents = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                evaluation = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return evaluation;
+        }
+
+        private static Evaluation CreateObject(SqlDataReader reader)
+        {
+            return new Evaluation
+            {
+                Activity = int.Parse(dr[""])
+            };
+        }
+    }
+}
